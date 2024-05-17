aa5e9731461fba49b67326474eff0a5fa9dcf71d
wdy <1273171041@QQ.com>
2024-05-17 22:08:19 +0800

    mapper 创建

--- src/main/java/com/cafuc/mapper/UserMapper.java
+++ src/main/java/com/cafuc/mapper/UserMapper.java
@@ -5,9 +5,5 @@ import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper {
-   /* @Select("select * from user where uid=#{U_id} AND uPassword =#{U_password}")
-    @Results(id = "userMap",value = {
-            @Result(id = true,column ="uId",property = "U_id]")
-            }
-    )*/
+
 }
